6af2280cd99123065cd7ff8d24f71d26b226a515
rp0984082 <rp0984082@otc.edu>
2025-01-28 12:57:49 -0600

    Step 1 StudentsJSON

---
+++ src/Student.java
@@ -0,0 +1,13 @@
+import com.google.gson.Gson;
+import java.io.FileWriter;
+import java.io.FileWriter;
+import java.lang.reflect.Type;
+
+public class Student {
+        public static void main(String[] args) {
+            int id;
+            String name;
+            int age;
+            String grade;
+        }
+}
